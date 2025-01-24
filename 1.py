185fbbf732e294c3ea7d3472fd3b4afdf3696be5
Raj Shekhar Mehra <159541887+RAJMEHRA6261@users.noreply.github.com>
2025-01-24 19:57:31 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+a = 100
+b = 200
+"""print("The value of a is ",a)
+print("The value of b is ",b)"""
+
+#print("The value of a is {} and the value of b is {}".format(a,b))
+#print("The value of a is {0} and the value of b is {1}".format(a,b))
+#print("The value of a is {1} and the value of b is {0}".format(a,b))
+
+print(f"The value of a is {a} and value of b is {b}")
+
